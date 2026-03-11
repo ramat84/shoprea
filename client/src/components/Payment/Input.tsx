4f54f3fc00205462b6123259bd718f6585d5f194
spot <ram.mat904@gmail.com>
2026-03-11 17:12:55 +0200

    Select + filter (countries)

--- client/src/components/Payment/Input.tsx
+++ client/src/components/Payment/Input.tsx
@@ -1,6 +1,85 @@
-export const Input = ({ name, label }: { name: string, label: string }) => (
-    <fieldset className="inputText">
-        <label htmlFor={name}>{label}</label>
-        <input type="text" id={name} name={name} required />
-    </fieldset>
-)
+import { useEffect, useState } from "react"
+
+export const Input = ({ name, label, values = [] }: { name: string, label: string, values: any }) => {
+    const [filter, setFilter] = useState('')
+    const [valueText, setValueText] = useState('')
+    const [inputValue, setInputValue] = useState('')
+
+    const SelectValue = ({ target }) => {
+        const selectedSelect = target
+        const selectedOption = selectedSelect.selectedOptions[0]
+        setValueText(selectedOption.text)
+        setInputValue('')
+    }
+
+    const UpdateInputValue = () => {
+        setFilter(event.target.value)
+        setInputValue(event?.target.value)
+    }
+
+    const KeyUp = (e) => {
+        const selectSelector = `select[name=${e.target.name}_value]`;
+
+        switch (e.key) {
+
+            case 'Enter':
+                const selectElement = document.querySelector(selectSelector)
+
+                SelectValue({ target: selectElement });
+                selectElement.focus();
+                break;
+
+            case 'ArrowDown':
+                const nextOption = document.querySelector(selectSelector + " option:checked ~ option")
+                if (nextOption) {
+                    nextOption.selected = true;
+                }
+                break;
+
+            case 'ArrowUp':
+                const prevOption = document.querySelector(selectSelector + " option:has(+option:checked)")
+                if (prevOption) {
+                    prevOption.selected = true;
+                }
+                break;
+        }
+    }
+
+    const Options = () => {
+        let isFirst = true;
+        return values.map((value) => {
+            const show = (filter == '' || value.name.toLowerCase().indexOf(filter.toLowerCase()) >= 0)
+            const selected = show && isFirst;
+            if (selected) isFirst = false;
+            return show && (
+                <option
+                    key={value.code}
+                    value={value.code}
+                    selected={selected}>
+                    {value.name}
+                </option>
+            )
+        })
+    }
+
+    return (
+        <fieldset className="inputText">
+            <label htmlFor={name}>{label}</label>
+            <input
+                type="text"
+                id={name}
+                name={name}
+                value={inputValue}
+                required={valueText == ''}
+                placeholder={valueText}
+                onChange={UpdateInputValue}
+                onKeyUp={KeyUp}
+            />
+            {values.length > 0 && (
+                <select name={name + '_value'} size="5" onChange={SelectValue} >
+                    <Options />
+                </select>
+            )}
+        </fieldset>
+    )
+}
--- client/src/components/Payment/Shipment.tsx
+++ client/src/components/Payment/Shipment.tsx
@@ -1,8 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Input } from "./Input";
 
-export const Shipment = () => (
-    <form>
-        <Input name="country" label="Country" />
+export const Shipment = () => {
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+        let url = 'http://localhost:4000/api/location/countries'
+        axios.get(url).then((res) => {
+            setCountries(res.data)
+        })
+
+    }, [])
+
+    return <form>
+        <Input name="country" label="Country" values={countries} />
         <Input name="city" label="City" />
         <Input name="address" label="Address" />
         <Input name="phone" label="Phone Number" />
@@ -10,4 +22,4 @@ export const Shipment = () => (
         <Input name="zip" label="Zip Code" />
         {/* TODO: Method  */}
     </form>
-)
+}
